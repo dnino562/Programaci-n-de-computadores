5f878d0b2ef37ae9db5e695ecf2ff0944c5dd677
dnino562 <dnino562@unab.edu.co>
2026-03-11 11:10:02 -0500

    Add files via upload

---
+++ Meta.py
@@ -0,0 +1,9 @@
+from clases import Personaje
+
+mi_personaje = Personaje("Danna",5,80)
+
+mi_personaje.mostrar_estado()
+mi_personaje.atacar()
+mi_personaje.descansar()
+mi_personaje.subir_nivel()
+mi_personaje.mostrar_estado()
---
+++ clases.py
@@ -0,0 +1,31 @@
+class Personaje:
+    def __init__(self, nombre, nivel=1, energia=100):
+        self.nombre = nombre
+        self.nivel = nivel
+        self.energia = energia
+
+    def atacar(self):
+        self.energia = -10
+        print(f"{self.nombre} atacar. Energía restante: {self.energia}")
+
+    def descansar(self):
+        self.energia = 100
+        print(f"{self.nombre} descansa. Energía restaurada a {self.energia}")
+
+    def mostrar_estado(self):
+        print("Estado de Personaje")
+        print(f"Nombre: {self.nombre}")
+        print(f"Nivel: {self.nivel}")
+        print(f"Energía: {self.energia}")
+    
+    def subir_nivel(self):
+        self.nivel += 1
+        print(f"{self.nombre} subió de nivel. Nuevo nivel: {self.nivel}")
+
+mi_personaje = Personaje("Danna",5,80)
+
+mi_personaje.mostrar_estado()
+mi_personaje.atacar()
+mi_personaje.descansar()
+mi_personaje.subir_nivel()
+mi_personaje.mostrar_estado()
