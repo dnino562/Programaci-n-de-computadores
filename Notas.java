08d6997d326f1391cb841c41452ade7d8ccf8808
dnino562 <dnino562@unab.edu.co>
2026-04-24 11:57:34 -0500

    Add files via upload

---
+++ Notas.java
@@ -0,0 +1,14 @@
+package pck;
+import java.util.Scanner;
+public class Notas {
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+	int numeros=numeros [5];
+	System.out.println("Ingrese 5 notas: ");
+	for (int i = 0 ; i < 5 ; i++) {
+	int numeros=sc.nextInt();
+	System.out.println(numeros);
+	}
+	}
+
+}
